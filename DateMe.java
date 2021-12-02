2372a6113382cb14910ec0d3e1d9653d65057fc0
Sathaphorn Phansiri <clicktamproject@gmail.com>
2021-12-02 16:11:42 +0700

    Add files via upload

--- DateMe.java
+++ DateMe.java
@@ -1,25 +1,31 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class DateMe {
     public static void main(String[] args) throws ParseException {
         //        java.util.Calendar c = java.util.Calendar.getInstance();
-                SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         //        df.setTimeZone(TimeZone.getTimeZone("GMT+7"));
         //        c.add(Calendar.YEAR, 543);
-                String currentDate = df.format(new Date());
-                System.out.printf("currentDate " + currentDate);
+        String currentDate = df.format(new Date());
+        System.out.printf("currentDate " + currentDate);
         //        String currentDate = "02/12/2021 12:00:10";
-        
-                SimpleDateFormat df2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-                df2.setTimeZone(TimeZone.getTimeZone("GMT+7"));
-                System.out.println(df2.parse(currentDate));
-        
-                Date d1 = null;
-                Calendar c = Calendar.getInstance();
-                TimeZone thaiTimeZone = TimeZone.getTimeZone("Asia/Kolkata");
-        
-                c.setTimeZone(thaiTimeZone);
-                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-                d1 = sdf.parse(currentDate);
-                c.setTime(d1);
-                System.out.println(d1);
-            }
+
+        SimpleDateFormat df2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        df2.setTimeZone(TimeZone.getTimeZone("GMT+7"));
+        System.out.println(df2.parse(currentDate));
+
+        Date d1 = null;
+        Calendar c = Calendar.getInstance();
+        TimeZone thaiTimeZone = TimeZone.getTimeZone("Asia/Kolkata");
+
+        c.setTimeZone(thaiTimeZone);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        d1 = sdf.parse(currentDate);
+        c.setTime(d1);
+        System.out.println(d1);
+    }
 }
